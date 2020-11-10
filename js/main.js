6cc3200bb35740933dacf4e603e6c2551973dbc1
Marco Giulietti <ga6t00n@gmail.com>
2020-11-10 11:41:22 +0100

    HTML & CSS

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * CREATE A TODOLIST WITH VUE
+ * Creare un nuovo todo attraverso un campo di testo
+ * Rimuovere uno specifico todo attraverso il click su un icona per la rimozione
+ * nel todo stesso
+ * 
+ */
+
