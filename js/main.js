50b42be786531f907a8d2f2cb86bc4e6c8bb6ccd
Marco Giulietti <ga6t00n@gmail.com>
2020-11-11 11:09:59 +0100

    Ultimi dettagli
    Pulizia codice, innesto alcune utilities, pulizia css

--- js/main.js
+++ js/main.js
@@ -13,6 +13,9 @@ var app = new Vue({
       listIndex: 'none',  
       icon: 'fas fa-check-circle',
       nuova: '',
+      numDay: '',
+      numToday: '',
+      numMonth: '',
       todos: [{
           nome: 'Fare la spesa',
           done: ''},
@@ -26,12 +29,48 @@ var app = new Vue({
           nome: 'Portare a spasso il cane',
           done: ''}]
     },
+
+    created() {
+            const d = new Date();
+            let settimana = new Array(7);
+            settimana[0] = "Domenica";
+            settimana[1] = "Lunedì";
+            settimana[2] = "Martedì";
+            settimana[3] = "Mercoledì";
+            settimana[4] = "Giovedì";
+            settimana[5] = "Venerdì";
+            settimana[6] = "Sabato";
+            const date = settimana[d.getDay()];
+            let mese = new Array(12);
+            mese[0] = "Gen.";
+            mese[1] = "Feb.";
+            mese[2] = "Mar.";
+            mese[3] = "Apr.";
+            mese[4] = "Mag.";
+            mese[5] = "Giu.";
+            mese[6] = "Lug.";
+            mese[7] = "Ago.";
+            mese[8] = "Set.";
+            mese[9] = "Ott.";
+            mese[10] = "Nov.";
+            mese[11] = "Dic.";
+            const mesi = mese[d.getMonth()];
+            const oggi = d.getDate();
+            const day = date;
+            this.numDay = day;
+            this.numToday = oggi;
+            this.numMonth = mesi;
+    },
+
     methods: {
         uploadTask() {
-            this.todos.push({
-                nome: this.nuova,
-                done: ''
-              })
+
+            if (this.nuova.trim() !== '') {
+                this.todos.push({
+                    nome: this.nuova,
+                    done: ''
+                  })
+            }
             this.nuova = '';
         },
 
@@ -47,6 +86,6 @@ var app = new Vue({
                 this.listIndex = index;
                 this.todos[index].done = 'fatto';
             }
-       }
+        }
     }
 });
